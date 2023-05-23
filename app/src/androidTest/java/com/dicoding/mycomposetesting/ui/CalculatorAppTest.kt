b9fe03be6adcf75a534f7295bd7b49ebca248b0f
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-23 20:57:22 +0700

    create: initial commit

---
+++ app/src/androidTest/java/com/dicoding/mycomposetesting/ui/CalculatorAppTest.kt
@@ -0,0 +1,53 @@
+package com.dicoding.mycomposetesting.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasNoClickAction
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.dicoding.mycomposetesting.ui.theme.MyComposeTestingTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import com.dicoding.mycomposetesting.R
+
+class CalculatorAppTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent { 
+            MyComposeTestingTheme {
+                CalculatorApp()
+            }
+        }
+    }
+
+    @Test
+    fun calculate_area_of_rectangle_correct() {
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count), useUnmergedTree = true)
+            .assertHasNoClickAction()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 12.0))
+            .assertExists()
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_length))
+            .performTextInput("..3")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.enter_width))
+            .performTextInput("4")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.count))
+            .performClick()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.result, 0.0))
+            .assertExists()
+    }
+}
